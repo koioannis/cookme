b7e7fdb73cadd274f4f27fa3e4d35ca56ce59893
alex <giannisproff733@hotmail.com>
2020-11-21 12:34:31 +0200

    Added more frontend

--- server/src/subscribers/user.js
+++
@@ -1,9 +0,0 @@
-const { Container } = require('typedi');
-const { EventSubscriber, On } = require('event-dispatch');
-const mongoose = require('mongoose');
-const events = require('./events');
-
-@EventSubscriber()
-class UserSubscriber {
-  
-}
