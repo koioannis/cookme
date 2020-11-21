a3657d8ca205a0f53c11261a933f8ea452f82cd6
alex <alexandros441@yahoo.gr>
2020-11-21 12:29:11 +0200

    Old files

---
+++ server/src/models/user/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: false,
+    min: 6,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 1024,
+  },
+  salt: {
+    type: String,
+    required: true,
+    max: 32,
+  },
+  _userInfo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'UserInfo',
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
