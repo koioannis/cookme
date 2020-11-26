1b27c609fa75157056640a9d2016622aa6e13c1c
Ioannis Kontopoulos <giannisproff733@hotmail.com>
2020-11-26 20:57:14 +0200

    fixed a typo in signout route

--- server/src/api/ApiRoutes.js
+++ server/src/api/ApiRoutes.js
@@ -1,16 +1,13 @@
-const config = require('../config');
-
-const ApiBaseUrl = config.apiUrl;
 const BasePrefix = '/api';
 
-const Base = `${ApiBaseUrl + BasePrefix}/v1`;
+const Base = `${BasePrefix}/v1`;
 const AuthBase = `${Base}/auth`;
 const AccountBase = `${Base}/account`;
 
 const ApiRoutes = {
   SignUp: `${AuthBase}/signup`,
   SignIn: `${AuthBase}/signin`,
-  Signout: `${AuthBase}/signout`,
+  SignOut: `${AuthBase}/signout`,
   ForgotPassword: `${AccountBase}/forgot-password`,
   ResetPassword: `${AccountBase}/reset-password`,
 };
