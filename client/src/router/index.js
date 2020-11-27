1fe5eecbf504c6c4c488461d1843456fffa8c126
alex <alexandros441@yahoo.gr>
2020-11-27 20:49:13 +0200

    Added About,Home, Changed App.vue, Fixed some code

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,20 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-import { App } from '@/App.vue';
+import Home from '@/views/Home.vue';
+import About from '@/views/About.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'App',
-    component: App,
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
   },
   {
     path: '*',
-    name: 'App',
-    component: App,
+    name: 'Home',
+    component: Home,
   },
 ];
 
---
+++ client/src/views/About.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="about">
+    <Navbar style="top: 2%"/>
+    <SignTemplate />
+    <Instruction />
+
+    <div class="header-wrapper">
+      <HeaderInfo />
+    </div>
+
+    <b-row style="background-color: #F7F7F7;height: 20em">
+      <b-col sm="4" class="text-center">
+        asd
+      </b-col>
+      <b-col sm="8" class="question-section text-left">
+        asd
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/landing/Navbar.vue';
+import HeaderInfo from '@/components/about/HeaderInfo.vue';
+import SignTemplate from '@/components/auth/SignTemplate.vue';
+import Instruction from '@/components/landing/Instruction.vue';
+
+export default {
+  name: 'About',
+  components: {
+    Navbar,
+    HeaderInfo,
+    SignTemplate,
+    Instruction,
+  },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  #about {
+    margin: auto;
+  }
+
+  .header-wrapper {
+    position: relative;
+    height: 32em;
+  }
+</style>
