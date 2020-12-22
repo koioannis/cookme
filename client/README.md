19c88aae062ea69c330c1de80441ac4e9b515a00
alex <giannisproff733@hotmail.com>
2020-12-22 19:03:15 +0200

    Added rating

--- client/README.md
+++ client/README.md
@@ -21,25 +21,40 @@
 
 ## Project Steps
 
-1. [ ] Landing Page
+1. Landing Page
     - [ ] Authentication
         - [x] Sign In
         - [x] Sign Up
         - [x] Forgot Password
         - [ ] Add functionality
     - [x] How does it work
-2. [ ] About
+    - [ ] Interact with posts
+2. About
     - [x] Create all the different endpoints
     - [x] Add more endpoints
     - [x] Create Contact form
-    - [ ] Make it more responsive
+    - [x] Make it more responsive
+    - [ ] Add Contact Logic
     - [ ] Optimize the code
-3. [ ] Discover
+3. Discover
     -  SignedIn
-        - [ ] View Posts
-        - [ ] Interact with the posts
+        - [x] View Posts
+        - [x] Link Posts
+        - [x] Interact with the posts
     - SignedOut
-        - [ ] View Posts
+        - [x] View Posts
+    - [ ] Responsive
+    - [ ] Add Logic
+        - [ ] Search Bar
+        - [ ] Show Different Posts
+4. Post
+    - [ ] Show Information
+    - [ ] Make Responsive
+    - [ ] Optimize the Code
+    - [ ] Add Logic
+        - [ ] Ingredients Check
+        - [ ] Comments
+        - [ ] Likes / Starts
 
 ## Conclusion
 
--- client/src/components/posts/PostSection.vue
+++ client/src/components/posts/PostSection.vue
@@ -2,12 +2,22 @@
   <div class="mt-5 ml-5">
     <b-row>
       <b-col cols="8">
-        <div class="d-flex justify-content-between" style="width: 90%">
-          <h3 class="pb-3 recipe-title"><b>Μακαρόνια Μπογιαμπες</b></h3>
-          <div class="pb-3">asd</div>
-        </div>
-        <img src="@/assets/carousel/image_1.jpg" style="width: 90%"
-          class="shadow-lg mt-4 rounded">
+        <div style="width: 90%">
+          <div class="d-flex justify-content-between">
+            <h3 class="pb-3 recipe-title"><b>Μακαρόνια Μπογιαμπες</b></h3>
+            <div class="pb-3">asd</div>
+          </div>
+
+          <img src="@/assets/carousel/image_1.jpg" class="shadow-lg mt-4 rounded img-fluid">
+          <div class="d-flex mt-4 mb-4 justify-content-between">
+            <div>
+              <h6>Βαθμολογίστε το!</h6>
+              <img v-for="key in 5" :key="key"
+                :src="[key<=star ? require('@/assets/svg/yellow_star.svg'):
+                require('@/assets/svg/empty_star.svg')]" :class="{'ml-2': key !== 1}" id="stars"
+                @mouseover="star = key" @mouseleave="star = 0">
+            </div>
+          </div>
 
           <div class="description mt-4 mb-4">
             <h5 class="mt-4"><b>Περιγραφή</b></h5>
@@ -29,18 +39,39 @@
               τις κρυμμένες ικανότητες που έχεις.
             </div>
           </div>
+        </div>
       </b-col>
 
-      <b-col class="mt-5 pt-5">
+      <b-col class="mt-5 pt-4">
         <div class="d-flex ingredients align-items-center pt-2 pb-2">
           <img src="@/assets/svg/ingredients.svg">
           <h3 class="ml-3 mt-1 ingredients-title"><b>Υλικά</b></h3>
-          <h5 class="ml-auto mt-2 pr-3" style="color: #F19721"><b>€€€</b></h5>
+          <h5 class="ml-auto mt-2 pr-3" style="color: #676767"><b>€€€</b></h5>
         </div>
 
+        <!-- TODO -->
         <b-row class="mt-4 text-center" style="width: 65%">
-          <b-col class=""><b><u>Όνομα</u></b></b-col>
-          <b-col><b><u>Ποσότητα</u></b></b-col>
+          <b-col>
+            <b><u>Όνομα</u></b>
+            <div class="d-flex mt-3 ml-2">
+              <img src="@/assets/svg/empty_box.svg" style="width: 18%">
+              <div class="ml-2">Κακάο</div>
+            </div>
+            <div class="d-flex mt-3 ml-2">
+              <img src="@/assets/svg/empty_box.svg" style="width: 18%">
+              <div class="ml-2">Ζάχαρι</div>
+            </div>
+            <div class="d-flex mt-3 ml-2">
+              <img src="@/assets/svg/empty_box.svg" style="width: 18%">
+              <div class="ml-2">Κακάο</div>
+            </div>
+          </b-col>
+          <b-col>
+            <b><u>Ποσότητα</u></b>
+            <div class="mt-3">1gr</div>
+            <div class="mt-3">200gr</div>
+            <div class="mt-3">100gr</div>
+          </b-col>
         </b-row>
       </b-col>
     </b-row>
@@ -50,6 +81,11 @@
 <script>
 export default {
   name: 'PostSection',
+  data() {
+    return {
+      star: 0,
+    };
+  },
 };
 </script>
 
@@ -58,6 +94,11 @@ export default {
     white-space: nowrap;
     border-bottom: 2px solid #EF6D9A;
     width: 28%;
+  }
+
+  #stars {
+    cursor: pointer;
+    width: 15%
   }
 
   .ingredients {
