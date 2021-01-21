394272fd5e20305f6790995eb599fdbff8354508
Ioannis Kontopoulos <giannisproff733@hotmail.com>
2021-01-21 14:54:46 +0200

    refactor: fixed a bug on login

--- server/src/models/user/User.js
+++ server/src/models/user/User.js
@@ -26,6 +26,7 @@ const userSchema = new mongoose.Schema({
   isAdmin: {
     type: Boolean,
     required: true,
+    deafult: false,
   },
   refreshTokens: [{
     type: mongoose.Schema.Types.ObjectId,
