d4dd636fcc8a0c022bb966e8d3b2381a7c0b786a
alex <alexandros441@yahoo.gr>
2021-01-29 13:48:38 +0200

    optimize: discovery navbar

--- client/src/components/discovery/SignSideBar.vue
+++ client/src/components/discovery/SignSideBar.vue
@@ -2,15 +2,15 @@
   <div class="position-fixed d-lg-flex d-none flex-column justify-content-center">
     <div class="font-weight-bold h4 notification-label">Ειδοποιήσεις</div>
     <div class="align-items-center d-flex flex-column text-center">
-      <div v-if="logedIn">
+      <div v-if="loggedIn">
+        Δεν έχετε καμία ειδοποίηση.
+      </div>
+      <div v-else>
         <div class="text-muted signin-info">
           Συνδεθείτε για να μπορείτε να<br />υποστηρίξετε τους φίλους σας.
         </div>
         <b-button size="md" class="custom-button mt-2"
           @click="$bvModal.show('sign-in')">Σύνδεση</b-button>
-      </div>
-      <div v-else>
-        Δεν έχετε καμία ειδοποίηση.
       </div>
     </div>
   </div>
@@ -20,8 +20,8 @@
 export default {
   name: 'SignSideBar',
   computed: {
-    logedIn() {
-      return this.$store.getters['auth/logedIn'];
+    loggedIn() {
+      return this.$store.getters['auth/loggedIn'];
     },
   },
 };
